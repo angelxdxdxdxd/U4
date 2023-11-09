19ec96aa42b2ea8df0859aebe2691c01216847d7
angelxdxdxdxd <143724173+angelxdxdxdxd@users.noreply.github.com>
2023-11-09 08:51:43 -0600

    Add files via upload

---
+++ ArregloCalif.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ArregloCalif {
+    public static double promedio(calificaciones[0][0], calificaciones[0][0])
+    public static void main(String[] args) {
+        double[][] calificaciones = {
+            {90.5, 85.0, 78.5, 92.0},
+            {88.0, 76.5, 89.0, 94.5},
+            {70.0, 82.5, 91.0, 87.5}
+        };
+        
+        int [] numeros = {1,2,3};
+        String [] nombres = {"Sandra", "Pedro", "Juan"};
+        
+        int [] arreglo = {1,2,3,4};
+        String [] materias = {"F.P Calculo M.D F.I"};
+            
+        
+        
+        for (int i = 0; i < 5; i++){
+            for (int o = 0; o < 4; o++){
+                System.out.println(materias[0]);
+                System.out.println(nombres[0] + " " + calificaciones[0][0] + " " + calificaciones[0][1] + " " + calificaciones[0][2] + " " + calificaciones[0][3]);
+                System.out.println(nombres[1] + " " + calificaciones[1][0] + " " + calificaciones[1][1] + " " + calificaciones[1][2] + " " + calificaciones[1][3]);
+                System.out.println(nombres[2] + " " + calificaciones[2][0] + " " + calificaciones[2][1] + " " + calificaciones[2][2] + " " + calificaciones[2][3]);
+            }
+        System.out.println();
+    }
+}
+}
